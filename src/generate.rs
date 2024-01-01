5149aaa805f31bf981db8d83b2a19822e780a8cd
Cypher121 <8020624+Cypher121@users.noreply.github.com>
2024-01-02 00:42:23 +0100

    Make utility functions make some sense maybe

--- src/generate.rs
+++ src/generate.rs
@@ -27,10 +27,10 @@ pub fn generate(w: u32, h: u32, seed: u64) -> Board {
   (0..w).for_each(|x| {
     (0..h).for_each(|y| {
       let directions =
-          get_single_direction_set(&horizontal_edges, Direction4::North, x, y) |
-              get_single_direction_set(&horizontal_edges, Direction4::South, x, y + 1) |
-              get_single_direction_set(&vertical_edges, Direction4::West, x, y) |
-              get_single_direction_set(&vertical_edges, Direction4::East, x + 1, y);
+          get_single_direction_set(horizontal_edges.get_or_default(x, y), Direction4::North) |
+              get_single_direction_set(horizontal_edges.get_or_default(x, y + 1), Direction4::South) |
+              get_single_direction_set(vertical_edges.get_or_default(x, y), Direction4::West) |
+              get_single_direction_set(vertical_edges.get_or_default(x + 1, y), Direction4::East);
 
       grid.insert(Coord::new(x, y), Cell::new(directions).spin(rng.i32(0..4)));
     })
@@ -44,16 +44,22 @@ fn generate_edge(rng: &mut Rng, x: u32, y: u32) -> Edge {
   return Edge::from(rng.bool())
 }
 
-fn get_single_direction_set(edges: &Grid<Edge>, direction: Direction4, x: u32, y: u32) -> Direction4Set {
-  if get_edge(edges, x, y) == Lit {
+fn get_single_direction_set(edge: Edge, direction: Direction4) -> Direction4Set {
+  if edge == Lit {
     Direction4Set::from(direction)
   } else {
     Direction4Set::empty()
   }
 }
 
-fn get_edge(edges: &Grid<Edge>, x: u32, y: u32) -> Edge {
-  edges.get(Coord::new(x, y)).copied().unwrap_or_default()
+trait GetOrDefaultByCoords<T : Default> {
+  fn get_or_default(&self, x: u32, y: u32) -> T;
+}
+
+impl<T> GetOrDefaultByCoords<T> for Grid<T> {
+  fn get_or_default(&self, x: u32, y: u32) -> T {
+    self.get(Coord::new(x, y)).copied().unwrap_or_default()
+  }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
@@ -72,4 +78,3 @@ impl From<bool> for Edge {
     }
   }
 }
-
