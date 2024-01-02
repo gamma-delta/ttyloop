7590a32edfb3f40df358d41a53397558396bc171
Cypher121 <8020624+Cypher121@users.noreply.github.com>
2024-01-02 01:30:59 +0100

    fix fox fax

--- src/generate.rs
+++ src/generate.rs
@@ -24,16 +24,14 @@ pub fn generate(w: u32, h: u32, seed: u64) -> Board {
 
   let mut grid = Grid::new(w, h);
 
-  (0..w).for_each(|x| {
-    (0..h).for_each(|y| {
-      let directions =
-          get_single_direction_set(horizontal_edges.get_or_default(x, y), Direction4::North) |
-              get_single_direction_set(horizontal_edges.get_or_default(x, y + 1), Direction4::South) |
-              get_single_direction_set(vertical_edges.get_or_default(x, y), Direction4::West) |
-              get_single_direction_set(vertical_edges.get_or_default(x + 1, y), Direction4::East);
+  grid.area().into_iter().for_each(|Coord { x, y }| {
+    let directions =
+      get_single_direction_set(horizontal_edges.get_or_default(x, y), Direction4::North) |
+        get_single_direction_set(horizontal_edges.get_or_default(x, y + 1), Direction4::South) |
+        get_single_direction_set(vertical_edges.get_or_default(x, y), Direction4::West) |
+        get_single_direction_set(vertical_edges.get_or_default(x + 1, y), Direction4::East);
 
-      grid.insert(Coord::new(x, y), Cell::new(directions).spin(rng.i32(0..4)));
-    })
+    grid.insert(Coord::new(x, y), Cell::new(directions).spin(rng.i32(0..4)));
   });
 
   return Board::new(grid);
@@ -52,11 +50,11 @@ fn get_single_direction_set(edge: Edge, direction: Direction4) -> Direction4Set 
   }
 }
 
-trait GetOrDefaultByCoords<T : Default> {
+trait GetOrDefaultByCoords<T> where T: Default, T: Copy {
   fn get_or_default(&self, x: u32, y: u32) -> T;
 }
 
-impl<T> GetOrDefaultByCoords<T> for Grid<T> {
+impl<T> GetOrDefaultByCoords<T> for Grid<T> where T: Default, T: Copy {
   fn get_or_default(&self, x: u32, y: u32) -> T {
     self.get(Coord::new(x, y)).copied().unwrap_or_default()
   }
